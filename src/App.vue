082eda9757f8cbe67c258bf235c41a8015fff50a
Riccardo <riccardorocco2001@gmail.com>
2023-11-27 20:38:45 +0100

    fine

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+  La mia prima app con Vite
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <img src="https://picsum.photos/seed/picsum/200" alt="">
+</template>
+
+<style scoped>
+</style>
